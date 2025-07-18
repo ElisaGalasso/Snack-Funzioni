ff55daff850257d0bc5941f0c395535586625ced
“ElisaGalasso“ <galassoelisa72@gmail.com>
2025-07-18 17:40:51 +0200

    complete script.js bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -6,13 +6,28 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
-
+let orario = "18:00"
 
 // Dichiara la funzione qui.
+
+function salutoUtente (name, orario) {
+    if (orario < "13:00") {
+        return`Buongiorno ${name}`;
+
+    } else if (orario < "17:00" ){
+        return `Buon pomeriggio ${name}`
+    
+    } else {
+        return `Buonasera ${name}`
+
+    }
+    
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(salutoUtente (name, orario));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
