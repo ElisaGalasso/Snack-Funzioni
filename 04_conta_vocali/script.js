57a6a5ed695aa20391581667b099e4015949987d
“ElisaGalasso“ <galassoelisa72@gmail.com>
2025-07-18 17:02:12 +0200

    complete script.js conta_vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,30 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function contaVocali (word) {
+    let conteggio = 0
+    let vocali = []
+    for (let i = 0; i < word.length; i++) {
+        const lettera = word [i]
+        if (lettera === "a" ||
+            lettera === "e" ||
+            lettera === "i" ||
+            lettera === "o" ||
+            lettera === "u" ) {
+            
+            conteggio++;
+            vocali.push (lettera)
+        }
+
+    }
+
+    return {conteggio, vocali: vocali.join (", ")}
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(contaVocali (word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
