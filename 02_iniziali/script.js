fa1fe7f3e3953d7e9534a740f1a50a5c45f69510
“ElisaGalasso“ <galassoelisa72@gmail.com>
2025-07-18 15:12:18 +0200

    complete script.js iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -6,9 +6,21 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function prendiIniziali (names) {
+    let iniziali = []
+    for (let i= 0; i < names.length; i++) {
+        iniziali.push (names[i] [0]);
+    }
+    return iniziali
+}
+
+
+//function arrow
+
+//const prendiIniziali = (names) => names.map (names => names [0])
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(prendiIniziali(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
